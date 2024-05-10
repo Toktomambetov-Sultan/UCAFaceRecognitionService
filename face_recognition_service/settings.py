3ee70c35272f06ed2d9edaf0d4c30e6e6dcfdc3d
Toktomambetov-Sultan <sultan.toktomambetov.04@bk.ru>
2024-05-10 16:04:32 +0600

    cors headers

--- face_recognition_service/settings.py
+++ face_recognition_service/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = 'django-insecure-8d60sr)3_ia1%bb0!r_zocct^b#=1@x*janzpeyrwr+7&+a(^b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 ALLOWED_HOSTS = ["*","0.0.0.0"]
 
 
@@ -39,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'corsheaders',
     'rest_framework'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
